4d5afc357e430108b10e68829efdaf68f5332b83
Luke Przekwas <galactica@The-Galactica.local>
2018-01-26 15:42:27 -0600

    added substring on film list to descriptions

--- src/components/FilmCards.jsx
+++ src/components/FilmCards.jsx
@@ -10,7 +10,7 @@ class FilmCards extends React.Component {
                     <div className="card-body">
                         <h5 className="card-title">{this.props.title}</h5>
                         <h6 className="card-subtitle mb-2 text-muted">{this.props.release_date} | RT Score: {this.props.rt_score}</h6>
-                        <p className="card-text">{this.props.description}</p>
+                        <p className="card-text">{this.props.description.substring(0, 150)} ...</p>
                         <p className="text-muted">Director: {this.props.director} | Producer: {this.props.producer}</p>
                         <Link className="d-block btn btn-primary mt-2" to={"/films/" + this.props.link}>Show Only This Film</Link>
                     </div>
